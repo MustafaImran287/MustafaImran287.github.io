4a757c2a84149df08ad7ed0a63487efcf44628b7
MustafaImran57 <mustafa.imran@addqual.com>
2026-01-06 12:44:12 +0000

    Fix basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
+const basePath = process.env.NODE_ENV === "production" ? "/EimanKalsoom.github.io" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
